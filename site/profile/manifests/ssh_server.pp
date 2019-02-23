6aa6bd3e1e30d6f06806997e53502473313d461e
Ingvar-V <ingvarvolkov@gmail.com>
2019-02-23 22:28:11 +0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCvfT9tLcdGHz/WHXvXl68A6q5VHgDFE2YqrSXNE0eKn90+9xuZJOvRgk8qoa+5N4tRiEUFaPDOOnIe2mv1ZlLE/sY+CIGcVH0XyPFnR4tPcQOyyHDyIggLr6K++Iy83tShbMU+DhXF7zFb2WkTcuCuVICrZVQ1whiHB5/1m6qIS+1WYRTh0LrdsMfeKjakpzblrub2NTbudw8g32IK3bIMopC7DX788ubRNs/lSugcP3IQV4dIlo+OELSVWmXm08QAl2fJTyrHSxBFVZD/SrOj96lRlC6otKsl/oTgNZNvaKbuFs1R96zCSq4Ktmkyarx6hZQdovjql8wXKR8Fky2X',
 	}  
 }
